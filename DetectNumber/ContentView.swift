882d38edb5d92e1d520eaaf430db9ee84ec0e5cb
Alexey Duryagin <aiduryagih@gmail.com>
2020-05-11 16:07:45 +0300

    Initial Commit

---
+++ DetectNumber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DetectNumber
+//
+//  Created by Алексей Дурягин on 11.05.2020.
+//  Copyright © 2020 Alexey Duryagin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
